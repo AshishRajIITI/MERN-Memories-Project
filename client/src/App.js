7a45648a96464933a17a8aff328ce3591de2bd66
AshishRajIITI <ashishiiti19@gmail.com>
2021-03-18 15:26:38 +0530

    till redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import memories from './images/memories.png';
-import {Container, AppBar, Typography,Grow,Grid } from '@material-ui/core';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import Form from './components/Form/Form.js';
 import Posts from './components/Posts/Posts.js';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 
-
-const App =() => {
+const App = () => {
     const classes = useStyles();
-    return(
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch])
+
+    return (
         <>
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center"> MEMORIES</Typography>
-                <img className={classes.image} src={memories} alt="pic is not available" height ="60"></img>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts />
+            <Container maxWidth="lg">
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                    <Typography className={classes.heading} variant="h2" align="center"> MEMORIES</Typography>
+                    <img className={classes.image} src={memories} alt="pic is not available" height="60"></img>
+                </AppBar>
+                <Grow in>
+                    <Container>
+                        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                            <Grid item xs={12} sm={7}>
+                                <Posts />
+                            </Grid>
                         </Grid>
-                    </Grid> 
-                    <Grid item xs={12} sm={4}>
-                        <Form />
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
+                    </Container>
+                </Grow>
+            </Container>
         </>
     );
 }
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Post from './Post/Post.js';
 import useStyles from './styles';
 
+
 const Posts = () =>{
     const classes = useStyles();
     return(
